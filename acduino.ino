594d0b524c314a547dcbe205d54198d7a7bd4134
David Salinas Fdz. de Land <ikarus@MacBook-Pro-de-David.local>
2016-07-18 03:40:56 +0200

    Visualización de posición y revoluciones en otro display 8x7 segmentos.

--- acduino.ino
+++ acduino.ino
@@ -8,11 +8,11 @@ int encoderB = LOW;
 int encoderPinALast = LOW;
 
 //Pin connected to ST_CP of 74HC595
-int latchPin = 8; //RCK MARRON
+int latchPin = 4; //RCK MARRON
 //Pin connected to SH_CP of 74HC595
-int clockPin = 10; //SCK ROJO
+int clockPin = 6; //SCK ROJO
 ////Pin connected to DS of 74HC595
-int dataPin = 9; // DIO NARANJA
+int dataPin = 5; // DIO NARANJA
 
 int point_numbers[10] = {
   B01000000
@@ -57,41 +57,67 @@ int refresh = 1;
 
 void setup()
 {
-	Serial.begin(50000);
-	//set pins to output because they are addressed in the main loop
-	pinMode(latchPin, OUTPUT);
-	pinMode(clockPin, OUTPUT);
-	pinMode(dataPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("ACDUINO STARTING...");
+  //set pins to output because they are addressed in the main loop
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
 }
 
 void loop()
 {
   while (Serial.available() > 0)
-	{
+  {
     Serial.readBytesUntil(delimiter, data, 80);
     String command = String(data);
-    setData(command);
-    //memset(data, '\0', 80);
-	} 
-  
+    processCommand(command);
+    memset(data, '\0', 80);
+  }
 }
 
-void setData(String cadena)
+void processCommand(String command)
 {
+  Serial.println(command);
+  int type = command.charAt(0);
+  setData(type - '0', command.substring(2, command.length()));
+}
+
+void setData(int panel, String cadena)
+{
+  Serial.print("Panel: "); Serial.print(panel); Serial.println("");
+  Serial.print("Texto: "); Serial.print(cadena); Serial.println("");
+  
   int posicion = 7;
   int digitos[8];
   
   for (int i=7; i>=0; i--)
-  {    
-    if (i != 2 && i != 4)
-    {
-      showData(cadena[i], posicion - i);
-    }
+  {
+    showData(panel, cadena[i], posicion - i);
   }
 }
 
-void showData(char digit, int posicion)
+void showData(int panel, int numero, int indice)
 {
+  switch (panel)
+  {
+    case 1:
+      showData1(numero, indice);
+      break;
+    case 2:
+      showData2(numero, indice);
+      break;
+    default:
+      break;
+  }
+}
+
+// MOSTRAR VUELTAS COMPLETADAS, MARCHA Y VELOCIDAD.
+
+void showData1(char digit, int posicion)
+{
+  if (3 == posicion || 5 == posicion) return;
+  
   int indice = digit - '0';
   
   if (posicion == 4)
@@ -104,10 +130,32 @@ void showData(char digit, int posicion)
       indice--;
   }
   
+  showPanel1(posicion, indice);
+}
+
+void showPanel1(int posicion, int indice)
+{
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, MSBFIRST, positions[posicion]);
   shiftOut(dataPin, clockPin, MSBFIRST, numbers[indice]);
   digitalWrite(latchPin,HIGH);
-  delay(refresh);  
+  delay(refresh); 
 }
 
+// MOSTRAR POSICIÓN Y REVOLUCIONES POR MINUTO
+void showData2(char digit, int posicion)
+{
+  if (posicion == 5) return;
+  
+  int indice = digit - '0';  
+  showPanel2(posicion, indice);
+}
+
+void showPanel2(int posicion, int indice)
+{
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, positions[posicion]);
+  shiftOut(dataPin, clockPin, MSBFIRST, numbers[indice]);
+  digitalWrite(latchPin,HIGH);
+  delay(refresh); 
+}
